40cf42e04f72a8ca28eb09efeeae6efaaafe1ecf
William Hleucka <william.hleucka@gmail.com>
2026-05-14 09:08:49 -0600

    fix: add recently added

--- app/Http/Controllers/Soprano/HomeController.php
+++ app/Http/Controllers/Soprano/HomeController.php
@@ -2,14 +2,31 @@
 
 namespace App\Http\Controllers\Welcome;
 
+use App\Services\Soprano\HomeService;
 use Echo\Framework\Http\Controller;
 use Echo\Framework\Routing\Route\Get;
 
 class HomeController extends Controller
 {
-    #[Get("/", "home.index")] 
-    public function index(): string
+    public function __construct(private HomeService $service) {}
+
+    #[Get("/", "home.root")] 
+    public function index(): void
+    {
+        redirect("/home")->send();
+    }
+
+    #[Get("/home", "home.index")] 
+    public function home(): string
     {
         return $this->render("home/index.html.twig");
+    }
+
+    #[Get("/home/recently-added", "home.recently-added")]
+    public function recentlyAdded(): string
+    {
+        return $this->render("home/recently-added.html.twig", [
+            "items" => $this->service->recentlyAdded(),
+        ]);
     }
 }
---
+++ app/Services/Soprano/HomeService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services\Soprano;
+
+use App\Models\TrackMeta;
+
+class HomeService
+{
+    public function recentlyAdded(int $album_count = 20): array
+    {
+        $recently_added = TrackMeta::where("id", ">", 0)
+            ->orderBy("created_at", "DESC")
+            ->groupBy("album")
+            ->get($album_count);
+        return array_map(fn($item) => [
+            "hash" => $item->track()->hash,
+            "title" => $item->title,
+            "artist" => $item->artist,
+            "album" => $item->album,
+            "cover" => $item->cover,
+            "year" => $item->year,
+        ], $recently_added);
+    }
+}
