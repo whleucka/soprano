c2fb8a6d9674181d50fb60bc60087c1c5ff5ad00
William Hleucka <william.hleucka@gmail.com>
2026-05-26 10:50:53 -0600

    fix: music config

--- config/soprano.php
+++ config/soprano.php
@@ -3,7 +3,7 @@
 $root = __DIR__ . "/../";
 
 return [
-    "music_path" => env("MUSIC_DIR"),
+    "music_path" => "/music",
     "covers_path" => $root . "storage/covers",
     "public_covers" => "/covers/",
 ];
