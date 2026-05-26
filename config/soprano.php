1128d32c747b50f6fbf6b08e3c879077bf91778d
William Hleucka <william.hleucka@gmail.com>
2026-05-26 10:35:20 -0600

    fix: add env MUSIC_DIR

--- config/soprano.php
+++ config/soprano.php
@@ -3,7 +3,7 @@
 $root = __DIR__ . "/../";
 
 return [
-    "music_path" => "/music",
+    "music_path" => env("MUSIC_DIR"),
     "covers_path" => $root . "storage/covers",
     "public_covers" => "/covers/",
 ];
