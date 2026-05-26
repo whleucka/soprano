d089561e67ab30ea90e8c2c58f6411f42a9e51d8
William Hleucka <william.hleucka@gmail.com>
2026-05-26 16:05:54 -0600

    fix: hidePlaylist

--- public/js/app.js
+++ public/js/app.js
@@ -23,10 +23,15 @@ function togglePlaylist() {
 function hidePlaylist() {
   var playlist = document.getElementById("playlist");
   var view = document.getElementById("view");
-  playlist.classList.add("d-none");
-  view.classList.remove("d-none");
-  view.classList.remove("d-lg-block");
-  updateActiveTrack();
+  // Mobile only
+  // if the view is hidden, then we should 
+  // hide the playlist and show the view
+  if (view.classList.contains("d-none")) {
+    playlist.classList.add("d-none");
+    view.classList.remove("d-none");
+    view.classList.remove("d-lg-block");
+    updateActiveTrack();
+  }
 }
 
 async function copyClipboard(e) {
