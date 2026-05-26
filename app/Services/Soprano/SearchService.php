f58cca85d5ce9a154342e8df27606b02edd2178c
William Hleucka <william.hleucka@gmail.com>
2026-05-26 16:17:19 -0600

    fix: limit search

--- app/Services/Soprano/SearchService.php
+++ app/Services/Soprano/SearchService.php
@@ -22,25 +22,24 @@ class SearchService
     public function search(string $term): array
     {
         $like = "%$term%";
-        $rows = db()->fetchAll(
-            "SELECT t.hash AS track_hash,
-                    al.hash AS album_hash,
-                    al.title AS album,
-                    al.cover AS cover,
-                    al.year AS year,
-                    ar.hash AS artist_hash,
-                    ar.name AS artist,
-                    tm.title AS title,
-                    tm.track_number AS track_number,
-                    tm.playtime_string AS playtime_string
-             FROM tracks t
-             JOIN albums al ON al.id = t.album_id
-             JOIN artists ar ON ar.id = t.artist_id
-             LEFT JOIN track_meta tm ON tm.track_id = t.id
-             WHERE ar.name LIKE ? OR al.title LIKE ? OR tm.title LIKE ?
-             ORDER BY ar.name, al.title, CAST(tm.track_number AS UNSIGNED)",
-            [$like, $like, $like],
-        );
+        $rows = db()->fetchAll("SELECT t.hash AS track_hash,
+            al.hash AS album_hash,
+            al.title AS album,
+            al.cover AS cover,
+            al.year AS year,
+            ar.hash AS artist_hash,
+            ar.name AS artist,
+            tm.title AS title,
+            tm.track_number AS track_number,
+            tm.playtime_string AS playtime_string
+        FROM tracks t
+        JOIN albums al ON al.id = t.album_id
+        JOIN artists ar ON ar.id = t.artist_id
+        LEFT JOIN track_meta tm ON tm.track_id = t.id
+        WHERE ar.name LIKE ? OR al.title LIKE ? OR tm.title LIKE ?
+        ORDER BY ar.name, al.title, CAST(tm.track_number AS UNSIGNED)
+        LIMIT 2500",
+        [$like, $like, $like]);
 
         return array_map(fn($row) => [
             'hash'            => $row['track_hash'],
