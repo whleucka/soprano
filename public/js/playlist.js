238c0b11f5ddf71091212425d73c3e89b5d40e8c
William Hleucka <william.hleucka@gmail.com>
2026-05-27 17:01:53 -0600

    fix: playlist toggle

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -4,20 +4,19 @@ function togglePlaylist() {
   var view = document.getElementById("view");
   playlist.classList.toggle("d-none");
   view.classList.toggle("d-none");
-  view.classList.toggle("d-lg-block");
+  view.classList.toggle("d-md-block");
   updateActiveTrack();
 }
 
 function hidePlaylist() {
   var playlist = document.getElementById("playlist");
   var view = document.getElementById("view");
-  // Mobile only
   // if the view is hidden, then we should 
   // hide the playlist and show the view
-  if (window.innerWidth <= 768) {
+  if (window.innerWidth <= 1200) {
     playlist.classList.add("d-none");
     view.classList.remove("d-none");
-    view.classList.remove("d-lg-block");
+    view.classList.remove("d-md-block");
     updateActiveTrack();
   }
 }
