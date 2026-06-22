97a2a235adb8b79c8f2780e0d6bd04ef7ef073a4
William Hleucka <william.hleucka@gmail.com>
2026-06-22 08:10:58 -0600

    fix: remove csp img-src

--- src/Framework/Http/Response.php
+++ src/Framework/Http/Response.php
@@ -83,7 +83,7 @@ class Response implements ResponseInterface
         header("X-Content-Type-Options: nosniff");
         header("Referrer-Policy: strict-origin-when-cross-origin");
         header("X-XSS-Protection: 1; mode=block");
-        header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'none'; font-src 'self'; connect-src 'self'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'");
+        header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; font-src 'self'; connect-src 'self'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'");
         if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
             header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
         }
